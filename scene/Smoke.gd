8be29f8aae9b906048c73121233a97f116639a1c
SamHolytoe <75861975+SamHolytoe@users.noreply.github.com>
2020-12-12 11:07:24 -0800

    Smoke particles and stuff.

---
+++ scene/Smoke.gd
@@ -0,0 +1,17 @@
+extends CanvasModulate
+var wavy = 0
+
+func _ready():
+	get_node(".").set_color(Color(1,1,1,0.5))
+	
+func _process(delta):
+	
+	if wavy == 0:
+		get_node(".").set_color(get_color() + Color(0,0,0,.005))
+		
+	if get_node(".").get_color().a > .6 or wavy == 1:
+		get_node(".").set_color(get_color() - Color(0,0,0,.005))
+		wavy = 1
+		
+	if wavy == 1 and (get_node(".").get_color().a < .15):
+		wavy = 0
