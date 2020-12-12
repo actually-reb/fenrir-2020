84a8268aa851d85edd23f8df5cd54382bcb5d8d6
SamHolytoe <75861975+SamHolytoe@users.noreply.github.com>
2020-12-12 13:53:31 -0800

    Smoke less annoying and added first few floor ideas.

--- scene/Smoke.gd
+++ scene/Smoke.gd
@@ -9,9 +9,9 @@ func _process(delta):
 	if wavy == 0:
 		get_node(".").set_color(get_color() + Color(0,0,0,.005))
 		
-	if get_node(".").get_color().a > .6 or wavy == 1:
+	if get_node(".").get_color().a > .4 or wavy == 1:
 		get_node(".").set_color(get_color() - Color(0,0,0,.005))
 		wavy = 1
 		
-	if wavy == 1 and (get_node(".").get_color().a < .15):
+	if wavy == 1 and (get_node(".").get_color().a < .1):
 		wavy = 0
