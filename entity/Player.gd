b240d92363d40f99277f11b9c9402ef0f065cf6a
actually-reb <61338113+actually-reb@users.noreply.github.com>
2020-12-11 19:30:17 -0800

    basic player movement

--- entity/Player.gd
+++ entity/Player.gd
@@ -1,7 +1,20 @@
 extends KinematicBody2D
 
+var speed = 300
+
 func _ready():
 	pass
 
 func _process(delta):
-	pass
+	var move = Vector2(0, 0)
+	if Input.is_action_pressed("move_up"):
+		move -= Vector2(0, 1)
+	if Input.is_action_pressed("move_down"):
+		move += Vector2(0, 1)
+	if Input.is_action_pressed("move_left"):
+		move -= Vector2(1, 0)
+	if Input.is_action_pressed("move_right"):
+		move += Vector2(1, 0)
+	move = move.normalized()
+	
+	move_and_slide(move * speed)
