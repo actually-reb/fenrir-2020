4f1f89eda9a393e6349e7ce4dbe6ca03f10ffafe
actually-reb <61338113+actually-reb@users.noreply.github.com>
2020-12-13 09:32:09 -0800

    powerup spacing and music volume

--- scene/Game.gd
+++ scene/Game.gd
@@ -54,7 +54,7 @@ func next_room():
 	Global.current_floor += 1
 	call_deferred("remove_child", room)
 	room.queue_free()
-	if Global.current_floor % 5 == 1:
+	if Global.current_floor % 4 == 0:
 		load_room(random_item_floor())
 	else:
 		load_room(random_floor())
